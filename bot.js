acccae773c5d086e9428fd17464399e890c5a752
Acezus <32319352+Acezus@users.noreply.github.com>
2017-10-29 22:29:35 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,19 +20,7 @@ client.on('message', (message) => {
 	    message.reply('Move along!');
     }
 	if(isCommand('Info', message)) {
-		let embed = new Discord.RichEmbed()
-    .setTitle("Help Panel")
-    .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-    .setThumbnail(message.guild.iconURL)
-    .setDescription("Look who needs help now. I'm only messing with ya! Should've saw your face. Anyway, commands are simple! Find any bugs? Direct message `Acezus#8392` and he should get it sorted out!")
-    .addField("Prefix", "**Prefix:** $$")
-    .addField("Commands", "**$$avatar** - To make this command work, make sure to mention someone.\n\n**$$help** - Using it right now.\n\n**$$server** - Will give you the infomation on the server! (members, regions, roles etc.)\n\n**$$userinfo** - You'll need to mention somewhere, to get their info, like what time they created the account.\n\n**$$followus** - A set of links to follow either Acezus or CanonOverseer.")
-    .addField("Moderation Commands", "**$$ban** - This command is only used for moderators, a quicker way to ban someone. Need to mention and give a reason why.\n\n**$$kick** - This command is only used for moderators, a quicker way to ban someone. Need to mention and give a reason why.\n\n**$$sug** - Where you suggest on what you think should be added to the bot!\n\n **$$panel** - Still a work in progress command, but only Staff Members can use it!")
-    .addField("CraneBot's Official Server", "Join us if you need help with anything: https://discord.gg/YFPmRH9");
-    
-    //Now we want it so it sends via DM!
-    message.author.sendEmbed(embed)
-	}
+	message.author.reply("Hey")
 });
 
 // THIS  MUST  BE  THIS  WAY
