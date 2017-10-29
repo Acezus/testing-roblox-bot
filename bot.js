306143a8ae8cc420846e6acad5c6650a930eb7c3
Acezus <32319352+Acezus@users.noreply.github.com>
2017-10-29 21:55:34 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,105 @@
-const Discord = require('discord.js');
-const roblox = require('roblox.js');
-const client = new Discord.Client();
+var discord = require('discord.js');
+var roblox = require('roblox-js');
+var client = new discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+roblox.login({username: "Revolutionary_BOT", password: "traforlifenerd"}).then((success) => {
+
+}).catch(() => {console.log("Sorry, it failed.");});
+
+
+client.on("ready", () => {
+  client.user.setGame(`Making HL3`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} joined ${guild}`)
 });
 
+client.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  let user = member.user
+  console.log(`${user.tag} left ${guild}`)
+});
+
+var prefix = '!';
+var groupId = 3356979;
+var maximumRank = 252;
+
+function isCommand(command, message){
+	var command = command.toLowerCase();
+	var content = message.content.toLowerCase();
+	return content.startsWith(prefix + command);
+}
+
+client.on('message', (message) => {
+	if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+    if(isCommand('Promote', message)){
+    	var username = args[1]
+    	if (username){
+    		message.channel.send(`Checking ROBLOX for ${username}`)
+    		roblox.getIdFromUsername(username)
+			.then(function(id){
+				roblox.getRankInGroup(groupId, id)
+				.then(function(rank){
+					if(maximumRank <= rank){
+						message.channel.send(`${id} is rank ${rank} and not promotable.`)
+					} else {
+						message.channel.send(`${id} is rank ${rank} and promotable.`)
+						roblox.promote(groupId, id)
+						.then(function(roles){
+							message.channel.send(`Promoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+						}).catch(function(err){
+							message.channel.send("Failed to promote.")
+						});
+					}
+				}).catch(function(err){
+					message.channel.send("Couldn't get him in the group.")
+				});
+			}).catch(function(err){ 
+				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+			});
+    	} else {
+    		message.channel.send("Please enter a username.")
+    	}
+    	return;
+    }
+
+    if(isCommand('Demote', message)){
+    	var username = args[1]
+    	if (username){
+    		message.channel.send(`Checking ROBLOX for ${username}`)
+    		roblox.getIdFromUsername(username)
+			.then(function(id){
+				roblox.getRankInGroup(groupId, id)
+				.then(function(rank){
+					if(maximumRank <= rank){
+						message.channel.send(`${id} is rank ${rank} and not demoteable.`)
+					} else {
+						message.channel.send(`${id} is rank ${rank} and demoteable.`)
+						roblox.demote(groupId, id)
+						.then(function(roles){
+							message.channel.send(`Demoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+						}).catch(function(err){
+							message.channel.send("Failed to Demote.")
+						});
+					}
+				}).catch(function(err){
+					message.channel.send("Couldn't get him in the group.")
+				});
+			}).catch(function(err){ 
+				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+			});
+    	} else {
+    		message.channel.send("Please enter a username.")
+    	}
+    	return;
+    }
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+roblox.login(process.env.USERNAME)
+roblox.login(process.env.PASSWORD)
